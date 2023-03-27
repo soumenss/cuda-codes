e0d756ea3c511502d2d95fc0ef26b658ffe75048
Soumen Sikder Shuvo <soumen123898@gmail.com>
2023-03-26 22:52:29 -0400

    Update test6.cu

--- Lab3/test6.cu
+++ Lab3/test6.cu
@@ -101,7 +101,7 @@ int main(int argc, char** argv) {
     cudaFree(d_kernel);
     cudaFree(d_output);
 
-
+/*
 
 
     // Print the input and output arrays
@@ -129,7 +129,7 @@ int main(int argc, char** argv) {
         printf("\n");
     }
 
-
+*/
 
     // Free host memory
     free(input);
