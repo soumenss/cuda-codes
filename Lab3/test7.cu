dffd015ceaf786d00d988eab4a9067354ffd83a9
Soumen Sikder Shuvo <soumen123898@gmail.com>
2023-03-26 20:19:00 -0400

    Update test7.cu

--- Lab3/test7.cu
+++ Lab3/test7.cu
@@ -3,9 +3,10 @@
 #include <stdlib.h>
 // #include <time.h>
 
-#define BLOCK_SIZE 16
-#define KERNEL_SIZE 3
-
+// #define BLOCK_SIZE 16
+// #define KERNEL_SIZE 3
+#define TILE_WIDTH 16
+#define MAX_KERNEL_RADIUS 3
 
 //__global__ void convolution2D_kernel(float* output, const float* input, const float* kernel,
  //                                   int dimx, int dimy, int dimk) {
