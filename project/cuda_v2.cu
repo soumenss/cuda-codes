d11f4a953a7b4656827889a7166b523599d49de7
Soumen Sikder Shuvo <soumen123898@gmail.com>
2023-04-27 18:20:07 -0400

    Update cuda_v2.cu

--- project/cuda_v2.cu
+++ project/cuda_v2.cu
@@ -1,17 +1,20 @@
 #include <cuda_runtime.h>
 #include <fstream>
+#include <iostream>
 
-__global__ void median_filter_kernel(const uchar* input, uchar* output, int width, int height, int kernel_size)
+#define BLOCK_SIZE 32
+
+__global__ void median_filter_kernel(const unsigned char* input, unsigned char* output, int width, int height, int kernel_size)
 {
     int padding_size = kernel_size / 2;
-    int index = threadIdx.x + blockIdx.x * blockDim.x;
-    int x = index % width;
-    int y = index / width;
-    int input_index, output_index;
-    uchar neighborhood[kernel_size * kernel_size];
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (x >= padding_size && x < width - padding_size && y >= padding_size && y < height - padding_size)
     {
+        unsigned char neighborhood[kernel_size * kernel_size];
+        int input_index, output_index = (y - padding_size) * (width - kernel_size + 1) + (x - padding_size);
+
         // Get the neighborhood
         for (int i = -padding_size; i <= padding_size; i++)
         {
@@ -29,7 +32,7 @@ __global__ void median_filter_kernel(const uchar* input, uchar* output, int widt
             {
                 if (neighborhood[i] > neighborhood[j])
                 {
-                    uchar temp = neighborhood[i];
+                    unsigned char temp = neighborhood[i];
                     neighborhood[i] = neighborhood[j];
                     neighborhood[j] = temp;
                 }
@@ -37,21 +40,20 @@ __global__ void median_filter_kernel(const uchar* input, uchar* output, int widt
         }
 
         // Write the output
-        output_index = (y - padding_size) * (width - kernel_size + 1) + (x - padding_size);
         output[output_index] = neighborhood[kernel_size * kernel_size / 2];
     }
 }
 
-void median_filter(const uchar* input, uchar* output, int width, int height, int kernel_size)
+void median_filter(const unsigned char* input, unsigned char* output, int width, int height, int kernel_size)
 {
     int padding_size = kernel_size / 2;
     int output_width = width - kernel_size + 1;
     int output_height = height - kernel_size + 1;
-    size_t input_size = width * height * sizeof(uchar);
-    size_t output_size = output_width * output_height * sizeof(uchar);
+    size_t input_size = width * height * sizeof(unsigned char);
+    size_t output_size = output_width * output_height * sizeof(unsigned char);
 
     // Allocate memory on the device
-    uchar* d_input, * d_output;
+    unsigned char* d_input, * d_output;
     cudaMalloc(&d_input, input_size);
     cudaMalloc(&d_output, output_size);
 
@@ -59,8 +61,8 @@ void median_filter(const uchar* input, uchar* output, int width, int height, int
     cudaMemcpy(d_input, input, input_size, cudaMemcpyHostToDevice);
 
     // Define the grid and block sizes
-    dim3 block_size(32, 32);
-    dim3 grid_size((width * height + block_size.x * block_size.y - 1) / (block_size.x * block_size.y));
+    dim3 block_size(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 grid_size((output_width + BLOCK_SIZE - 1) / BLOCK_SIZE, (output_height + BLOCK_SIZE - 1) / BLOCK_SIZE);
 
     // Call the kernel
     median_filter_kernel<<<grid_size, block_size>>>(d_input, d_output, width, height, kernel_size);
@@ -76,26 +78,26 @@ void median_filter(const uchar* input, uchar* output, int width, int height, int
 int main()
 {
     // Load the image
-    std::ifstream file("image.raw", std::ios::binary);
+    std::ifstream file("input.jpg", std::ios::binary);
     if (!file)
     {
         std::cerr << "Error: Could not open image file." << std::endl;
         return 1;
     }
+
     int width = 512;
     int height = 512;
-    uchar input[width * height];
+    unsigned char input[width * height];
     file.read(reinterpret_cast<char*>(input), width * height);
     file.close();
 
-    // Apply the median filter with a kernel size of
-
+    // Apply the median filter with a kernel size of 5
     int kernel_size = 5;
-    uchar output[(width - kernel_size + 1) * (height - kernel_size + 1)];
+    unsigned char output[(width - kernel_size + 1) * (height - kernel_size + 1)];
     median_filter(input, output, width, height, kernel_size);
 
     // Write the output image
-    std::ofstream output_file("output.raw", std::ios::binary);
+    std::ofstream output_file("output.jpg", std::ios::binary);
     output_file.write(reinterpret_cast<const char*>(output), (width - kernel_size + 1) * (height - kernel_size + 1));
     output_file.close();
 
